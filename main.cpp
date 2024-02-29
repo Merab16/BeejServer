2aa378521edb551531741468faf79d2651cdfd94
vboxuser <vboxuser@Ubuntu11.myguest.virtualbox.org>
2024-02-29 19:06:43 +0300

    Add SaveUsers

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include "Server.h"
-#include <exception>
+
 
 int main(void)
 {
     Server* server;
-    try {
         server = new Server();
-    }
-    catch (std::exception& ex) {
-        std::cout << ex.what();
+
         server->SaveUsers();
-    }
+    
 
-
+    delete server;
     return 0;
 }
 
